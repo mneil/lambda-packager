ee3fc457ab7ea41c6936e7c95d8193768bc8fe06
Michael Neil <mneil@mneilsworld.com>
2021-09-03 14:02:23 -0700

    Add some files based on a plan
    I copied the fingerprint code from aws cdk. I may pull in
    webpack and do tree shaking to pull out the fingerprint stuff because
    I really like their method for hashing folders... otherwise what
    is here copied and referenced is good enough for now I guess.

---
+++ lib/index.ts
@@ -0,0 +1,14 @@
+export { version } from "./version";
+
+/**
+ * Common lambda packaging things.
+ *
+ * 1. What type of project is it? Python, node, go, java, etc...?
+ * 2. Where are the dependencies declared? Usually easy except python makes it... not easy. Go, is there a mod file or not?
+ * 3. Create a temporary directory to work in
+ * 4. Copy source files to that directory
+ * 5. Install dependencies into that directory
+ * 6. Archive the directory
+ * 7. Upload that archive to S3 and give it a distinct name
+ * 8. Inject that name somewhere? That's up to the user. We'll simply output the distinct name to be used....
+ */
---
+++ lib/version.ts
@@ -0,0 +1,12 @@
+import { fingerprint } from "./fingerprint";
+
+export async function version(package_directory: string = "") {
+  if (package_directory == "") {
+    package_directory = process.cwd();
+  }
+  // cdk fingerprint(directory)
+  const sha = fingerprint(package_directory);
+  // TODO: get git sha
+
+  return sha.substring(0, 8);
+}
