4dc3d94b7d0decf94b30080ec45500d71c152e45
Michael Neil <mneil@mneilsworld.com>
2021-09-04 18:09:05 -0700

    Handle npm install in node manifest

--- lib/manifest/base.ts
+++ lib/manifest/base.ts
@@ -1,5 +1,7 @@
 import { debug } from '../debug';
+import { copy } from '../fs';
 import { spawn } from 'child_process';
+import * as path from 'path';
 
 /**
  * IManifest representation. A manifest defines what type
@@ -16,7 +18,7 @@ export interface IManifest {
  */
 export abstract class BaseManifest implements IManifest {
   readonly manifest: string;
-  private readonly _baseDir: string;
+  protected readonly _baseDir: string;
   constructor(manifestFile: string, baseDir: string) {
     this.manifest = manifestFile;
     this._baseDir = baseDir;
@@ -30,10 +32,11 @@ export abstract class BaseManifest implements IManifest {
    */
   protected async exec(
     cmd: string,
-    args: Array<string>,
-    options: any
-  ): Promise<string | void> {
+    args?: Array<string>,
+    options: any = {}
+  ): Promise<void> {
     return new Promise((resolve, reject) => {
+      debug('executing %s %s', cmd, args?.join(' '));
       const spawned = spawn(cmd, args, {
         // accept all options without question
         ...options,
@@ -41,8 +44,8 @@ export abstract class BaseManifest implements IManifest {
         // so that they can set variables and pass them in. #elitedangerous
         ...(!options.env ? {} : Object.assign({}, process.env, options.env)),
       });
-      spawned.stdout.on('data', debug);
-      spawned.stderr.on('data', debug);
+      spawned.stdout.on('data', (data) => debug(data.toString('utf8')));
+      spawned.stderr.on('data', (data) => debug(data.toString('utf8')));
       spawned.on('error', reject);
       spawned.on('close', (code) => {
         if (code == 0) {
@@ -51,6 +54,17 @@ export abstract class BaseManifest implements IManifest {
         return reject(new String(code));
       });
     });
+  }
+  /**
+   * Copy manifest file to the directory
+   * Useful if you need to copy the manifest
+   * to the destination
+   *
+   * @param dir
+   */
+  protected async copyManifest(dir: string) {
+    const packageFile = path.resolve(this._baseDir, this.manifest);
+    await copy(packageFile, path.join(dir, path.basename(packageFile)));
   }
   /**
    * Install dependencies
